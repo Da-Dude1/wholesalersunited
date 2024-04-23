5c7ee9f1a9864842a9bad7dd8409f1a21a986cb5
Da-Dude1 <72897796+Da-Dude1@users.noreply.github.com>
2024-04-22 20:10:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+let innerCursor = document.querySelector('.inner-cursor');
+let outerCursor = document.querySelector('.outer-cursor');
+let hamburger = document.querySelector('.hamburger');
+let navLinkContainer = document.querySelector('.nav-link-container');
+
+document.addEventListener('mousemove', moveCursor);
+
+function moveCursor(event){
+    let x = event.clientX;
+    let y = event.clientY;
+
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+}
+
+let links = Array.from(document.querySelectorAll("a"));
+
+links.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        innerCursor.classList.add("grow");
+    });
+    link.addEventListener("mouseleave", () => {
+        innerCursor.classList.remove("grow");
+    });
+});
+
+hamburger.addEventListener("click", () => {
+    let body = document.body;
+    
+    body.classList.toggle('nav-open');
+    navLinkContainer.classList.toggle('active');
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// JavaScript functions to control the pop-up
+function openPopup() {
+  document.getElementById('overlay').style.display = 'flex';
+}
+
+function closePopup() {
+  document.getElementById('overlay').style.display = 'none';
+}
+
+// Auto-expanding textarea
+function autoExpand(textarea) {
+  textarea.style.height = 'auto';
+  textarea.style.height = (textarea.scrollHeight) + 'px';
+}
